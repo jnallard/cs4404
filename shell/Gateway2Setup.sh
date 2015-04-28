2c5f23543501f2c81abc71334238585c158613c0
Yan Yan <yyan@wpi.edu>
2015-04-28 17:53:30 -0400

    combine scripts into one line

--- shell/Gateway2Setup.sh
+++ shell/Gateway2Setup.sh
@@ -1,5 +1,1 @@
-sudo ip route add 10.4.12.1 via 10.4.12.2
-sudo ip route add 10.4.12.2 via 10.4.12.2
-sudo ip route add 10.4.12.4 via 10.4.12.4
-sudo ip route add 10.4.12.5 via 10.4.12.4
-sudo ip route add 10.4.12.6 via 10.4.12.4
+sudo ip route add 10.4.12.1 via 10.4.12.2;sudo ip route add 10.4.12.2 via 10.4.12.2;sudo ip route add 10.4.12.4 via 10.4.12.4;sudo ip route add 10.4.12.5 via 10.4.12.4;sudo ip route add 10.4.12.6 via 10.4.12.4
