7724b34e0f3ceeabb80b1c3dcb34ec86a873881c
Yan Yan <yyan@wpi.edu>
2015-04-28 19:27:42 -0400

    change ip address

--- test1/test1Gateway2.sh
+++ test1/test1Gateway2.sh
@@ -1,1 +1,1 @@
-sshpass -p 'cs4404' ssh cs4404@10.4.12.1 -t "sh ~/shared/routeRecord.sh; sudo ~/shared/attackerGateway"
+sshpass -p 'cs4404' ssh cs4404@10.4.12.3 -t "sh ~/shared/routeRecord.sh; sudo ~/shared/attackerGateway"
--- test1/test1Victim.sh
+++ test1/test1Victim.sh
@@ -1,1 +1,1 @@
-sshpass -p 'cs4404' ssh cs4404@10.4.12.1 -t "sudo ~/shared/victimGateway"
+sshpass -p 'cs4404' ssh cs4404@10.4.12.6 -t "sudo ~/shared/victimGateway"
