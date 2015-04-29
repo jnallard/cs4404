bb8eed9fe2092820bd51423fdc7ee4df61ef32e8
jnallard <jnallard@wpi.edu>
2015-04-28 20:43:08 -0400

    blah

--- routeRecord.sh
+++ routeRecord.sh
@@ -1,7 +1,1 @@
-sudo iptables --flush
-sudo iptables -A FORWARD -p udp -j NFQUEUE --queue-num 0
-sudo iptables -A INPUT -p udp -j NFQUEUE --queue-num 0
-sudo iptables -A OUTPUT -p udp -j NFQUEUE --queue-num 0
-sudo iptables -A FORWARD -p 200 -j NFQUEUE --queue-num 0
-sudo iptables -A INPUT -p 200 -j NFQUEUE --queue-num 0
-sudo iptables -A OUTPUT -p 200 -j NFQUEUE --queue-num 0
+sudo iptables --flush;sudo iptables -A FORWARD -p udp -j NFQUEUE --queue-num 0;sudo iptables -A INPUT -p udp -j NFQUEUE --queue-num 0;sudo iptables -A OUTPUT -p udp -j NFQUEUE --queue-num 0;sudo iptables -A FORWARD -p 200 -j NFQUEUE --queue-num 0;sudo iptables -A INPUT -p 200 -j NFQUEUE --queue-num 0;sudo iptables -A OUTPUT -p 200 -j NFQUEUE --queue-num 0
--- shell/push_changes.sh
+++ shell/push_changes.sh
@@ -8,21 +8,21 @@ d='. cs4404@10.4.12.4:~/shared'
 e='. cs4404@10.4.12.5:~/shared'
 f='. cs4404@10.4.12.6:~/shared'
 echo 'Pushing onto shared...'
-sshpass -p 'cs4404' scp -r $a
+sshpass -p 'cs4404' scp -q -r $a
 sshpass -p 'cs4404' ssh cs4404@10.4.12.1 -t "cd ~/shared;make"
 
-sshpass -p 'cs4404' scp -r $b
+sshpass -p 'cs4404' scp -q -r $b
 sshpass -p 'cs4404' ssh cs4404@10.4.12.2 -t "cd ~/shared;make"
 
-sshpass -p 'cs4404' scp -r $c
+sshpass -p 'cs4404' scp -q -r $c
 sshpass -p 'cs4404' ssh cs4404@10.4.12.3 -t "cd ~/shared;make"
 
-sshpass -p 'cs4404' scp -r $d
+sshpass -p 'cs4404' scp -q -r $d
 sshpass -p 'cs4404' ssh cs4404@10.4.12.4 -t "cd ~/shared;make"
 
-sshpass -p 'cs4404' scp -r $e
+sshpass -p 'cs4404' scp -q -r $e
 sshpass -p 'cs4404' ssh cs4404@10.4.12.5 -t "cd ~/shared;make"
 
-sshpass -p 'cs4404' scp -r $f
+sshpass -p 'cs4404' scp -q -r $f
 sshpass -p 'cs4404' ssh cs4404@10.4.12.6 -t "cd ~/shared;make"
 echo "Done"
